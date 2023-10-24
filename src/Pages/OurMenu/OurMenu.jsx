8c910d0ac3ef8de514abf4d739be98ec4d17c1d7
Sharif-Miah <farhansharif141077@gmail.com>
2023-10-24 20:40:14 +0600

    cover img and title added

--- src/Pages/OurMenu/OurMenu.jsx
+++ src/Pages/OurMenu/OurMenu.jsx
@@ -1,11 +1,21 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "./Cover/Cover";
+import CoverImg from '../../assets/menu/banner3.jpg'
+import MenuItems from "../Home/MenuItems/MenuItems";
 const OurMenu = () => {
   return (
     <div>
       <Helmet>
         <title>Bistro Boss || Menu</title>
       </Helmet>
-      <h1>Our Menu Section</h1>
+      <Cover img={CoverImg}/>
+      <MenuItems/>
+      <Cover img={CoverImg}/>
+      <MenuItems/>
+      <Cover img={CoverImg}/>
+      <MenuItems/>
+      <Cover img={CoverImg}/>
+      <MenuItems/>
     </div>
   );
 };
