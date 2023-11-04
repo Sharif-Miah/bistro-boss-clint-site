249b9ba7165725d6624e80663b6756ca8aa741ef
Sharif-Miah <farhansharif141077@gmail.com>
2023-11-04 20:22:09 +0600

    some section added

---
+++ src/Hooks/UseMenu.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+
+const UseMenu = () => {
+
+    const [menu, setMenu] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect( ()=> {
+        fetch('menu.json')
+        .then(res => res.json())
+        .then(data => {
+            setLoading(false)
+            setMenu(data)
+        })
+    },[])
+
+    return [menu, loading]
+};
+
+export default UseMenu;
--- src/Pages/Home/MenuItems/MenuItems.jsx
+++ src/Pages/Home/MenuItems/MenuItems.jsx
@@ -1,19 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import TitleSheard from '../../../Component/TitleSheard';
 import SingleMenu from './SingleMenu';
+import UseMenu from '../../../Hooks/UseMenu';
 
 const MenuItems = () => {
+    const [menu] = UseMenu()
+    const popular = menu.filter(item => item.category === "popular")
 
-    const [menu, setMenu] = useState([])
-
-    useEffect( ()=> {
-        fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const filterMenu = data.filter(item => item.category === 'popular')
-            setMenu(filterMenu)
-        })
-    },[])
+    
 
     return (
         <section className='mb-16'>
@@ -24,7 +18,7 @@ const MenuItems = () => {
 
             <div className='grid  md:grid-cols-2 gap-6'>
                 {
-                    menu.map(item => <SingleMenu
+                    popular.map(item => <SingleMenu
                     key={item._id}
                     item={item}
 
